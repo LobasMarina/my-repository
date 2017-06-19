14c6c68c502cb6e2e335a4a826a6856a7ad662da
LobasMarina <miss.Lobas@yandex.ru>
2017-06-20 00:19:23 +0300

    Update DEV-1.java

--- DEV-1.java
+++ DEV-1.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
 
-        for (int a=0; a<100; a++) {
+        for (int a=0; a<=100; a++) {
             if (a%3==0 && a!=0){
                 int N = a / 3;
                 System.out.print(" 3*" + N);
