39f21e93032e3bf2c8a5cb41433f71c6d19e2a1b
Marina <miss.Lobas@yandex.ru>
2017-06-20 22:58:35 +0300

    homework DEV-1

---
+++ DEV-1.java
@@ -0,0 +1,11 @@
+public class DEV_1 {
+    public static void main(String[] args) {
+        for (int a = 0; a <= 100; a++) {
+            if (a % 3 == 0) {
+                int N = a / 3;
+                System.out.print(" 3*" + N);
+            } else
+                System.out.print(" " + a);
+        }
+    }
+}
