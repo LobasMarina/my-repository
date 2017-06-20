05116fb5fd8af6e902f852e4b93044c9a5d87668
Marina <miss.Lobas@yandex.ru>
2017-06-21 01:29:56 +0300

    homework DEV-2

---
+++ Dev2.java
@@ -0,0 +1,15 @@
+public class Dev2 {
+    public static void main(String[] args) {
+        for (int a = 0; a <= 100; a++) {
+            if (a % 3 == 0 && a % 5 == 0) {
+                System.out.println("Tutti-Frutti");
+            } else if (a % 3 == 0) {
+                System.out.println("Tutti");
+            } else if (a % 5 == 0) {
+                System.out.println("Frutti");
+            } else {
+                System.out.println(a);
+            }
+        }
+    }
+}
