e5c88ef7eee131ecdce2c081667d9c65ffe30326
Marina <miss.Lobas@yandex.ru>
2017-06-24 17:53:24 +0300

    edit homework DEV-3

--- Fibonachi.java
+++ Fibonachi.java
@@ -2,26 +2,25 @@ import java.util.Scanner;
 
 public class Fibonachi {
   public static void main(String[] args) {
-    final int INT_LENGHT = 2147483647;
     System.out.print("Enter the number: ");
     Scanner in = new Scanner(System.in); 
+    int input_number = in.nextInt();
     boolean flag = true;
-    int a = 0;
-    int b = 1;
-    int x = 0;
-    int fib = in.nextInt();
-    for (int i = 0; x < INT_LENGHT; i++) {
-      x = a + b;
-      a = b;
-      b = x;
-      if (fib == x) {
-        System.out.print("Is a Fibonacci number");
+    int fib1 = 0;
+    int fib2 = 1;
+    int sum = 0;
+    for (int i = 0; sum < input_number; i++) {
+      sum = fib1 + fib2;
+      fib1 = fib2;
+      fib2 = sum;
+      if (input_number == sum) {
+        System.out.print("Is a Fibonachi number");
         flag = false;
         break;
         }
       }
       if (flag) {
-        System.out.print("This is not a Fibonacci number");
+        System.out.print("This is not a Fibonachi number");
       }
     }
   }
