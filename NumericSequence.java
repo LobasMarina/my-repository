3b5f26b456f26fb6e95dbdde5d11314df2483d67
Marina <miss.Lobas@yandex.ru>
2017-06-25 17:47:07 +0300

    edit homework DEV-4

--- NumericSequence.java
+++ NumericSequence.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Numeric_sequence {
+public class NumericSequence {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     System.out.print("Enter a sequence of numbers: ");
