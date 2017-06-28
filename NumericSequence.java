b23d2d760a5a53ed6b4a3fa3efaed7852e7ecbf7
Marina <miss.Lobas@yandex.ru>
2017-06-28 13:33:04 +0300

    edit2 homework DEV-4

--- NumericSequence.java
+++ NumericSequence.java
@@ -6,17 +6,12 @@ public class NumericSequence {
     Scanner input = new Scanner(System.in);
     String inputNumber = input.nextLine();
     String array[] = inputNumber.split(" ");
-    int i;
-      for (i = 0; i < array.length-1; i++) {
-        if (Integer.parseInt( array[i]) > Integer.parseInt( array[i+1])) {
-          continue;
-        } else {
-            System.out.println("The sequence is non-decreasing.");
-            break;
-          }
-      }
-      if (i ==  array.length-1) {
-        System.out.println("The sequence is decreasing.");
-      }
+      for (int i = 0; i < array.length-1; i++) {
+        if (Integer.parseInt( array[i+1]) > Integer.parseInt( array[i])) {
+          System.out.println("The sequence is non-decreasing.");
+          return;
+        }
+      }  
+    System.out.println("The sequence is decreasing.");
   }
 }
