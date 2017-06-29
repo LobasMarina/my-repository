78a07d60b11d92f60d9be21f3984639ea54c5231
Marina <miss.Lobas@yandex.ru>
2017-06-29 16:14:55 +0300

    modified DEV-4

--- NumericSequence.java
+++ NumericSequence.java
@@ -2,16 +2,23 @@ import java.util.Scanner;
 
 public class NumericSequence {
   public static void main(String[] args) {
+    if (args.length != 0) {
+      sequence(args);
+    } else {
     System.out.println("Enter a sequence of numbers separated by spaces: ");
     Scanner input = new Scanner(System.in);
     String inputNumber = input.nextLine();
     String array[] = inputNumber.split(" ");
+    sequence(array);
+    }
+  }
+    public static void sequence(String[] array) {
       for (int i = 0; i < array.length-1; i++) {
         if (Integer.parseInt( array[i+1]) > Integer.parseInt( array[i])) {
           System.out.println("The sequence is non-decreasing.");
           return;
         }
       }  
-    System.out.println("The sequence is decreasing.");
-  }
+      System.out.println("The sequence is decreasing.");
+    }
 }
