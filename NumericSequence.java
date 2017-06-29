9d242974a727ac6ebac098139db6ee6f8dcb3581
Marina <miss.Lobas@yandex.ru>
2017-06-29 20:16:24 +0300

    modified2 DEV-4

--- NumericSequence.java
+++ NumericSequence.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
 public class NumericSequence {
- /*the program determines that the user
- has entered a sequence of integers is non-decreasing*/
   public static void main(String[] args) {
     if (args.length != 0) {
       sequence(args);
@@ -14,16 +12,19 @@ public class NumericSequence {
     sequence(array);
     }
   }
-    public static void sequence(String[] array) {
-      /*the argument array a sequence to be analyzed, if the user 
-      has not transferred the sequence in the form of a command-line argument,
-      then a warning will be displayed*/
-      for (int i = 0; i < array.length-1; i++) {
-        if (Integer.parseInt( array[i+1]) > Integer.parseInt( array[i])) {
-          System.out.println("The sequence is non-decreasing.");
-          return;
-        }
-      }  
-      System.out.println("The sequence is decreasing.");
-    }
+/**
+ * The program determines that the user has entered a sequence of integers 
+ * is non-decreasing. the argument array a sequence to be analyzed, if the user 
+ * has not transferred the sequence in the form of a command-line argument,
+ * then a warning will be displayed
+ */
+  public static void sequence(String[] array) {
+    for (int i = 0; i < array.length-1; i++) {
+      if (Integer.parseInt( array[i+1]) > Integer.parseInt( array[i])) {
+        System.out.println("The sequence is non-decreasing.");
+        return;
+      }
+    }  
+    System.out.println("The sequence is decreasing.");
+  }
 }
