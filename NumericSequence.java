bba0103044c45b633d8a55edfef6cdbbcf80119b
Marina <miss.Lobas@yandex.ru>
2017-07-02 12:30:37 +0300

    modified3 DEV-5

--- NumericSequence.java
+++ NumericSequence.java
@@ -1,30 +1,37 @@
 import java.util.Scanner;
 
+/**
+ * The program determines that the user-entered sequence of integers is non-decreasing.
+ * The user can enter the sequence as a command-line argument.
+ * If the user doesn't enter the sequence as a command-line argument,
+ * the program requests a sequence from the console.
+ */
 public class NumericSequence {
+
   public static void main(String[] args) {
     if (args.length != 0) {
       sequence(args);
     } else {
-    System.out.println("Enter a sequence of numbers separated by spaces: ");
-    Scanner input = new Scanner(System.in);
-    String inputNumber = input.nextLine();
-    String array[] = inputNumber.split(" ");
-    sequence(array);
+      System.out.println("Enter a sequence of numbers separated by spaces: ");
+      Scanner input = new Scanner(System.in);
+      String inputNumber = input.nextLine();
+      String array[] = inputNumber.split(" ");
+      sequence(array);
     }
   }
-/**
- * The program determines that the user has entered a sequence of integers 
- * is non-decreasing. the argument array a sequence to be analyzed, if the user 
- * has not transferred the sequence in the form of a command-line argument,
- * then a warning will be displayed
- */
+
+  /**
+   * Defines the input sequence and writes the result to the console.
+   *
+   * @param array a sequence to be analyzed
+   */
   public static void sequence(String[] array) {
-    for (int i = 0; i < array.length-1; i++) {
-      if (Integer.parseInt( array[i+1]) > Integer.parseInt( array[i])) {
+    for (int i = 0; i < array.length - 1; i++) {
+      if (Integer.parseInt(array[i + 1]) > Integer.parseInt(array[i])) {
         System.out.println("The sequence is non-decreasing.");
         return;
       }
-    }  
+    }
     System.out.println("The sequence is decreasing.");
   }
 }
