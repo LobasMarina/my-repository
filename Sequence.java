0080f8a6e1053f44538d1e2fd0b5de3122faa72e
Marina <miss.Lobas@yandex.ru>
2017-07-07 13:27:31 +0300

    modified2 DEV-5

--- Sequence.java
+++ Sequence.java
@@ -1,3 +1,6 @@
+/**
+ * Analyzes integer sequences.
+ */
 public class Sequence {
   /**
    * Defines the input sequence and writes the result to the console.
@@ -5,9 +8,7 @@ public class Sequence {
    * @param args a sequence to be analyzed
    * @return true if sequence is non-decreasing, false if sequence is decreasing
    */
-
   public boolean sequenceCheck(String[] args) {
-
     for (int i = 0; i < args.length - 1; i++) {
       if (Integer.parseInt(args[i]) > Integer.parseInt(args[i + 1])) {
         return false;
