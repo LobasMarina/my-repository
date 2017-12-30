e07005f23409def651da65d228f2c2f0a78bc141
Marina <miss.Lobas@yandex.ru>
2017-12-31 02:25:24 +0300

    Task about brackets of location is added

---
+++ AlgebraicExpression/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("62ddbe82-59e6-4f87-808a-c6e623334d2a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AlgebraicExpression/Tests/UnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgebraicExpression;
+
+namespace Tests
+{
+  [TestClass]
+  public class UnitTest
+  {
+    [TestMethod]
+    public void TestExpressionWithDifferentBrackets()
+    {
+      Assert.AreEqual(true, new CheckerValidBracket("{[()]}").CheckLocationOfBrackets());
+    }
+
+    [TestMethod]
+    public void TestAlgebraicExpression()
+    {
+      Assert.AreEqual(true, new CheckerValidBracket("[2+(4-3)]").CheckLocationOfBrackets());
+    }
+
+    [TestMethod]
+    public void TestCorrectGetBrackets()
+    {
+      Assert.AreEqual("([]{})", new CheckerValidBracket("(7+3[9-5]+2{14-3})").GetBrackets());
+    }
+
+    [TestMethod]
+    public void TestExpressionWithOneBracket()
+    {
+      Assert.AreEqual(false, new CheckerValidBracket("[").CheckLocationOfBrackets());
+    }
+
+    [TestMethod]
+    public void TestExpressionWithWrongLocationBrackets()
+    {
+      Assert.AreEqual(false, new CheckerValidBracket("{[)])").CheckLocationOfBrackets());
+    }
+  }
+}
