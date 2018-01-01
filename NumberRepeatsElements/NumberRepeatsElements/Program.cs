647c49fe3209885635d5debe539dfda5aea8610b
Marina <miss.Lobas@yandex.ru>
2018-01-01 16:17:40 +0300

    Task about number of repeats in the collection is added

---
+++ NumberRepeatsElements/NumberRepeatsElements/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberRepeatsElements
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<int> collection = new List<int>() { 1, 2, 2, 3, 3, 3, 4, 5, 5, 5, 5, 6, 6 };
+      CheckerRepeatsElements repeat = new CheckerRepeatsElements(collection);
+      Console.Write(repeat.SearchRepeatingElements());
+    }
+  }
+}
