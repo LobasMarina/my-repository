811271dd814f4cfbdf5cfadc38f9acdaca593d01
Marina <miss.Lobas@yandex.ru>
2018-01-01 16:26:03 +0300

    Task about number of repeats in the collection is added

---
+++ NumberRepeatsElements/NumberRepeatsElements/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberRepeatsElements
+{
+  /// <summary>
+  /// Entry point of program.
+  /// </summary>
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<int> collection = new List<int>() { 1, 2, 2, 3, 3, 3, 4, 5, 5, 5, 5, 6, 6 };
+      CheckerRepeatsElements checker = new CheckerRepeatsElements(collection);
+      Console.Write(checker.SearchRepeatingElements());
+    }
+  }
+}
---
+++ NumberRepeatsElements/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using NumberRepeatsElements;
+
+namespace UnitTestProject
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestList()
+    {
+      Assert.AreEqual(4, new CheckerRepeatsElements(new List<int>() { 1, 2, 2, 3, 3, 3, 4, 5, 5, 5, 5, 6, 6 }).SearchRepeatingElements());
+    }
+    [TestMethod]
+    public void TestEmptyList()
+    {
+      Assert.AreEqual(0, new CheckerRepeatsElements(new List<int>()).SearchRepeatingElements());
+    }
+
+    [TestMethod]
+    public void TestOneElementList()
+    {
+      Assert.AreEqual(0, new CheckerRepeatsElements(new List<int>() { 3 }).SearchRepeatingElements());
+    }
+  }
+}
