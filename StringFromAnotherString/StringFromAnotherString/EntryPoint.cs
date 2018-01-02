d2a2ad135e77c37586f3ba6f648b6d5358f3934c
Marina <miss.Lobas@yandex.ru>
2018-01-03 00:02:31 +0300

    Task about about possible create one string using symbols from another is added

---
+++ StringFromAnotherString/StringFromAnotherString/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringFromAnotherString
+{
+  /// <summary>
+  /// Entry point of program.
+  /// </summary>
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      string firstString = "string";
+      string secondString = "substring";
+      StringAnalyzer analyzer = new StringAnalyzer(firstString, secondString);
+      Console.Write(analyzer.CheckPossibleCreateStringUsingSymbolsFromAnother());
+    }
+  }
+}
---
+++ StringFromAnotherString/StringFromAnotherString/StringAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace StringFromAnotherString
+{
+  /// <summary>
+  /// This class is used to check whether it is possible to create a string using the character of another string.
+  /// </summary>
+  public class StringAnalyzer
+  {
+    string firstString;
+    string secondString;
+
+    public StringAnalyzer(string firstString, string secondString)
+    {
+      this.firstString = firstString;
+      this.secondString = secondString;
+    }
+
+    /// <summary>
+    /// Check one string can be creating using symbols from another.
+    /// </summary>
+    /// <returns> true if possible create string using symbols from another string, false otherwise </returns>
+    public bool CheckPossibleCreateStringUsingSymbolsFromAnother()
+    {
+      bool isPossible = false;
+      foreach (char symbol in firstString)
+      {
+        if (secondString.Contains(symbol))
+        {
+          secondString = secondString.Remove(secondString.IndexOf(symbol), 0);
+          isPossible = true;
+        }
+        else
+        {
+          isPossible = false;
+          break;
+        }
+      }
+      return isPossible;
+    }
+  }
+}
